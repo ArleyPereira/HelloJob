728cf05e2db75d389903747e9ee383a5c9c53693
Arley Santana <arley.santana@hellodev.com.br>
2025-01-19 15:36:07 -0300

    Removendo modulos nao utilizados

--- main-home/src/main/java/br/com/hellodev/main/presenter/features/search/state/SearchState.kt
+++
@@ -1,9 +0,0 @@
-package br.com.hellodev.main.presenter.features.search.state
-
-import br.com.hellodev.common.domain.model.job.item.JobItemDomain
-
-data class SearchState(
-    val isLoading: Boolean = true,
-    val query: String = "",
-    val jobs: List<JobItemDomain> = emptyList()
-)
